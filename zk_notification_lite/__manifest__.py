ac2ad74dd01358b267e638a330bdaf9ca922d8bc
zekrom-tech <zekromtechllc@gmail.com>
2026-01-02 14:31:32 +0530

    price

--- zk_notification_lite/__manifest__.py
+++ zk_notification_lite/__manifest__.py
@@ -21,6 +21,8 @@
     """,
     'author': 'Zekrom Tech',
     'website': 'https://www.zekromtech.com',
+    'price': 15.00,
+    'currency': 'USD',
     'license': 'LGPL-3',
     'depends': ['base', 'web'],
     'data': [
